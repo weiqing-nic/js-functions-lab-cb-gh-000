6f999c31e6e1a1d48baa9850619c60449543a67f
weiqing lee <yoweiqing@gmail.com>
2018-05-29 10:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// write your code below!
 
+// write your code below!
 function happyHolidays(){
-  return 'Happy holidays!'
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function happyCountdown(holiday, days){
-  return `It's ${days} until ${holiday}!`;
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
 }
